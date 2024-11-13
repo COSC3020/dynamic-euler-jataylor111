7a91a95e599f2104c0232bcea288560224523147
jataylor111 <143449478+jataylor111@users.noreply.github.com>
2024-11-13 15:46:43 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,28 @@
+// function factorial(n) {
+//     if(n === 0) return 1;
+//     else return n * factorial(n - 1);
+// }
+
+// function e(n) {
+//     if(n === 0) return 1;
+//     else return 1.0 / factorial(n) + e(n - 1);
+// }
+
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+    let fact = 1;
+    // Change to use for loop 
+    for (let i = 1; i <= n; i++) { // Runs along all of n, complexity n
+        fact *= i;
+    }
+    return fact;
 }
 
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    let euler = 1.0;
+    // Change to for loop
+    // Delete Later: check with Ali, seems too simple to be right
+    for (let i = 1; i <= n; i++) { // Runs along all of n, complexity n
+        euler += (1.0 / factorial(i)); 
+    }
+    return euler;
 }
